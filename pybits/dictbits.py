91744999b088e93ca7a2f5773ca27b9057536e28
Bernhard Esperester <bernhard@esperester.de>
2021-04-22 11:47:28 +0200

    fixes circular import

--- pybits/dictbits.py
+++ pybits/dictbits.py
@@ -1,144 +1,10 @@
-import fnmatch
+from fnmatch import fnmatch
 
-from typing import Dict, List, Optional
-
-from pybits import listbits, exceptions
-
-
-def trim(item: Dict) -> Dict:
-    result = {}
-
-    for key, value in item.items():
-        if isinstance(value, dict):
-            result[key] = "{...}"
-        elif isinstance(value, list):
-            result[key] = "[...]"
-        else:
-            result[key] = value
-
-    return result
-
-
-def assert_is_equal(subset: Dict, superset: Dict, depth: int = 0) -> bool:
-    try:
-        if len(list(superset.keys())) != len(list(subset.keys())):
-            # early return false
-            # if length of superset keys and subset keys ar not equal
-            raise exceptions.ComparisonError(
-                f"Excpected length of keys to be equal"
-            )
-
-        # compare all keys
-        results = []
-        compare_exceptions = []
-
-        for key, value in subset.items():
-            if isinstance(value, dict):
-                # compare value recursive with assert_is_equal
-                # if value is an instance of dict
-                superset_value_dict: Dict = superset.get(key, {})
-
-                assert_is_equal(value, superset_value_dict, depth + 1)
-            elif isinstance(value, list):
-                # compare value recursive with listbits.assert_is_equal
-                # if value is an instance of list
-                superset_value_list: List = superset.get(key, [])
-
-                listbits.assert_is_equal(value, superset_value_list, depth + 1)
-            # elif isinstance(value, str):
-            #     # compare value
-            #     # if value is neither dict or list
-            #     superset_value_str: str = superset.get(key, "")
-
-            #     if not fnmatch.fnmatch(superset_value_str, value):
-            #         raise exceptions.ComparisonError(
-            #             f"Excpected '{value}' to match '{superset_value_str}'",
-            #             depth,
-            #         )
-            else:
-                # compare value
-                # if value is neither dict or list
-                superset_value = superset.get(key)
-
-                if value != superset_value:
-                    raise exceptions.ComparisonError(
-                        f"Excpected '{value}' to be equal to '{superset_value}'",
-                        depth,
-                    )
-    except exceptions.ComparisonError as e:
-        raise exceptions.ComparisonErrorInfo(
-            f"{trim(subset)} is not equal to {trim(superset)}: {e}", depth
-        )
-
-    # all compared keys must return true
-    # for the input to be a subset of superset
-    return True
-
-
-def is_equal(subset: Dict, superset: Dict) -> bool:
-    try:
-        return assert_is_equal(subset, superset)
-    except exceptions.ComparisonBaseError:
-        return False
-
-
-def assert_is_subset(subset: Dict, superset: Dict, depth: int = 0) -> bool:
-    # compare all keys
-    try:
-        for key, value in subset.items():
-            if isinstance(value, dict):
-                # compare value recursive with assert_is_subset
-                # if value is an instance of dict
-                superset_value_dict: Dict = superset.get(key, {})
-
-                assert_is_subset(value, superset_value_dict, depth + 1)
-            elif isinstance(value, list):
-                # compare value recursive with listbits.assert_is_subset
-                # if value is an instance of list
-                superset_value_list: List = superset.get(key, [])
-
-                listbits.assert_is_subset(
-                    value, superset_value_list, depth + 1
-                )
-            elif isinstance(value, str):
-                # compare value
-                # if value is neither dict or list
-                superset_value_str: str = superset.get(key, "")
-
-                if not fnmatch.fnmatch(superset_value_str, value):
-                    raise exceptions.ComparisonError(
-                        f"Excpected '{value}' to match '{superset_value_str}'",
-                        depth,
-                    )
-            else:
-                # compare value
-                # if value is neither dict or list
-                superset_value = superset.get(key)
-
-                if value != superset_value:
-                    raise exceptions.ComparisonError(
-                        f"Excpected '{value}' to be equal to '{superset_value}'",
-                        depth,
-                    )
-    except exceptions.ComparisonError as e:
-        raise exceptions.ComparisonErrorInfo(
-            f"{trim(subset)} is no subset of {trim(superset)}: {e}", depth
-        )
-
-    # all compared keys must return true
-    # for the input to be a subset of superset
-    return True
-
-
-def is_subset(subset: Dict, superset: Dict) -> bool:
-    try:
-        return assert_is_subset(subset, superset)
-    except exceptions.ComparisonBaseError:
-        return False
+from typing import Dict, List, Any, Optional
 
 
 def pick(
-    item: Dict,
+    item: Dict[str, Any],
     include: Optional[List[str]] = None,
     exclude: Optional[List[str]] = None,
     childrens_attribute_name: str = "children",
@@ -146,7 +12,7 @@ def pick(
     depth: int = -1,
     path: str = "*",
     path_attribute_name: str = "",
-) -> Dict:
+) -> Dict[str, Any]:
     if include is None:
         include = list(item.keys())
 
@@ -160,11 +26,11 @@ def pick(
     if childrens_attribute_name in include:
         include.remove(childrens_attribute_name)
 
-    result = {}
+    result: Dict[str, Any] = {}
 
     # add included attributes
     for attribute_name in include:
-        matches = [fnmatch.fnmatch(x, attribute_name) for x in item_keys]
+        matches = [fnmatch(x, attribute_name) for x in item_keys]
 
         if any(matches):
             match_index = matches.index(True)
@@ -174,7 +40,7 @@ def pick(
 
     # remove excluded attributes
     for attribute_name in exclude:
-        matches = [fnmatch.fnmatch(x, attribute_name) for x in result.keys()]
+        matches = [fnmatch(x, attribute_name) for x in result.keys()]
 
         if any(matches):
             match_index = matches.index(True)
@@ -186,7 +52,7 @@ def pick(
     if recursive:
         # make sure childrens attribute name exists in item keys
         if childrens_attribute_name in item_keys:
-            children = []
+            children: List[Dict[str, Any]] = []
 
             # make sure depth is either infinite or larger 0
             if depth == -1 or depth > 0:
@@ -208,15 +74,15 @@ def pick(
                     if len(parts) > 0:
                         first_part = parts[0]
 
-                        def children_filter(child: Dict) -> bool:
+                        def children_filter(child: Dict[str, Any]) -> bool:
                             if path_attribute_name in child.keys():
-                                return fnmatch.fnmatch(
+                                return fnmatch(
                                     child[path_attribute_name], first_part
                                 )
                             else:
                                 return False
 
-                        children = filter(children_filter, children)
+                        children = list(filter(children_filter, children))
 
                         # override path for next recursive step
                         path = "/".join(parts[1:])
--- pybits/exceptions.py
+++ pybits/exceptions.py
@@ -1,9 +1,6 @@
-from typing import List, Optional
-
-
 class ComparisonBaseError(Exception):
     def __init__(self, message: str, depth: int = 0) -> None:
-        self.depth = depth
+        self.depth: int = depth
 
         super().__init__(message)
 
